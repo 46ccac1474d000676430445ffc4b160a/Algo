135909e124cb396bb25199306829610d9e42c52d
User <46ccac1474d000676430445ffc4b160@mail.ru>
2018-04-28 18:01:20 +0600

    Update 18-04-28-18:01:20

--- commit.sh
+++ commit.sh
@@ -1,17 +1,24 @@
 #/bin/bash
 
-# git add '*.cbp'
-# git add '*.h'
-# git add '*.cpp'
-# git add '*.pro'
-# git add '*.doc'
-# git add '*.docx'
-# git add '*.txt'
-# git add '*.cbp'
-# git add '*.ico'
-# git add '*.qrc'
-# git add '*.zip'
-git add *
+git add '*.cbp'
+git add '*.h'
+git add '*.cpp'
+git add '*.pro'
+git add '*.doc'
+git add '*.docx'
+git add '*.txt'
+git add '*.cbp'
+git add '*.ico'
+git add '*.png'
+git add '*.qrc'
+git add '*.zip'
+git add '*.xml'
+git add '*.gradle'
+git add '*.properties'
+git add 'Lab3/Lab3/android/gradlew'
+git add '*.bat'
+git add '*.jar'
+git add '*.sh'
 
 git commit -m "Update "$(date +"%y-%m-%d-%T")
 
